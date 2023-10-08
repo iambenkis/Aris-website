5bb92b4e16db4461e3d177b705a158ab7105e5f6
benkis <benkisenge03@gmail.com>
2023-10-08 15:31:11 +0200

    Add and style navbar

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,39 @@
+import { MdElectricalServices } from 'react-icons/md'
+
+const Navbar = () => {
+  const links = [
+    { name: 'solutions', href: '' },
+    { name: 'services', href: '' },
+    { name: 'A propos', href: '' },
+    { name: 'contact', href: '' },
+  ]
+  return (
+    <nav className="w-full bg-red-200/25 flex justify-between px-[5rem] py-5">
+      <a
+        href="/"
+        className="w-full text-2xl flex items-center text-blue-900 font-bold flex-row"
+      >
+        <MdElectricalServices />
+        <span>COTEKI</span>
+      </a>
+      <ul className="flex w-[50%] justify-between">
+        {links.map((link, index) => (
+          <li>
+            <a
+              href={`${link.href}`}
+              className={`text-blue-900 uppercase text-xs font-link ${
+                link.name === 'contact'
+                  ? 'bg-blue-900 text-white px-3 py-2 rounded-full border-[1px] border-red-900'
+                  : ''
+              }`}
+            >
+              {link.name}
+            </a>
+          </li>
+        ))}
+      </ul>
+    </nav>
+  )
+}
+
+export default Navbar
