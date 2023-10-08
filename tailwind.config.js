98c7e1013f33979605c5dce30a57b63d415845f8
benkis <benkisenge03@gmail.com>
2023-10-08 15:30:33 +0200

    Install packages

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./src/**/*.{html,js,jsx}'],
+  theme: {
+    fontFamily: {
+      link: ['Nohemi', 'Trebuchet MS', 'sans-serif'],
+    },
+    extend: {},
+  },
+  plugins: [],
+}
