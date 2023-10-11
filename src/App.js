e0c9eb186acfdeef536d56ff282501e575826009
benkis <benkisenge03@gmail.com>
2023-10-11 15:39:56 +0200

    Import why and Footer section

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import Navbar from './components/Navbar'
 import Home from './components/Home'
+import Why from './components/Why'
+import Footer from './components/Footer'
 
 function App() {
   return (
     <div className="bg-neutral-100">
       <Navbar />
       <Home />
+      <Why />
+      <Footer />
     </div>
   )
 }
