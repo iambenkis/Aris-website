54f851a9f7f058fa266d9473cc270017b13b1c8f
benkis <benkisenge03@gmail.com>
2023-10-12 23:27:51 +0200

    Update tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,8 +4,24 @@ module.exports = {
   theme: {
     fontFamily: {
       link: ['Nohemi', 'Trebuchet MS', 'sans-serif'],
+      pop: ['Poppins', 'sans-serif'],
     },
-    extend: {},
+    extend: {
+      boxShadow: {
+        inner: 'inset 0 0 20px rgba(0, 0, 0, 0.8)',
+        // Add more custom shadow styles as needed
+      },
+    },
   },
   plugins: [],
 }
+
+const withMT = require('@material-tailwind/react/utils/withMT')
+
+module.exports = withMT({
+  content: ['./src/**/*.{js,jsx,ts,tsx}'],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+})
