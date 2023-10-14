7520df1d54ebd7f3980fbbd5142feb270978b289
benkis <benkisenge03@gmail.com>
2023-10-14 03:11:32 +0200

    Update testimony component

--- src/components/Testimony.jsx
+++ src/components/Testimony.jsx
@@ -25,9 +25,38 @@ function StarIcon() {
 }
 
 const Testimony = () => {
-  const items = ['Item 1', 'Item 2', 'Item 3', 'Item 4']
+  const items = [
+    {
+      name: 'Jean Dupont',
+      job: 'Ingénieur Électrique',
+      testimony:
+        "J'ai été extrêmement satisfait des services électriques fournis par cette entreprise. Leur équipe était compétente, efficace et professionnelle tout au long du processus.",
+      rating: 5,
+    },
+    {
+      rating: 3,
+      name: 'Marie Martin',
+      job: 'Ingénieur Mécanique',
+      testimony:
+        "Je recommande vivement cette entreprise pour tous les services mécaniques. Ils ont fait preuve d'un haut niveau d'expertise et de dévouement, fournissant des résultats exceptionnels dans les délais promis.",
+    },
+    {
+      name: 'Michel Dujardin',
+      job: 'Spécialiste des Télécommunications',
+      rating: 4,
+      testimony:
+        "J'ai été impressionné par la qualité des services de télécommunications offerts par cette entreprise. Leur souci du détail et leur engagement envers la satisfaction du client les distinguent vraiment dans l'industrie.",
+    },
+    {
+      name: 'Sophie Leroy',
+      job: "Technicienne en Installation d'Antennes",
+      rating: 5,
+      testimony:
+        "En tant que technicienne en installation d'antennes, j'ai collaboré avec cette entreprise sur plusieurs projets. La compétence technique et le professionnalisme de leur équipe ont systématiquement dépassé les attentes.",
+    },
+  ]
   return (
-    <div className="px-40 relative">
+    <div className="px-40 pb-10 relative">
       <h2 className="px-20 font-[RegularNohemi]  text-[2.4rem] text-blue-900 text-center font-medium">
         Ce que les gens disent de nous
       </h2>
@@ -38,53 +67,7 @@ const Testimony = () => {
         vous.
       </p>
       <div>
-        <Carousel items={items} />
-      </div>
-      <div className="py-10 px-40">
-        <Card
-          color="transparent"
-          shadow={false}
-          className="w-full flex flex-col items-center border px-10"
-        >
-          <CardHeader
-            color="transparent"
-            floated={false}
-            shadow={false}
-            className="mx-0 flex items-center gap-4 pt-0 pb-8"
-          >
-            <Avatar
-              size="lg"
-              variant="circular"
-              src="https://images.unsplash.com/photo-1633332755192-727a05c4013d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1480&q=80"
-              alt="tania andrew"
-            />
-            <div className="flex w-full flex-col gap-0.5">
-              <div className="flex items-center justify-between">
-                <Typography variant="h5" color="blue-gray">
-                  Tania Andrew {` `}
-                </Typography>
-                <div className="5 flex items-center gap-0">
-                  {` `}
-                  <StarIcon />
-                  <StarIcon />
-                  <StarIcon />
-                  <StarIcon />
-                  <StarIcon />
-                </div>
-              </div>
-              <Typography color="blue-gray">
-                Ingenieur en telecomunication
-              </Typography>
-            </div>
-          </CardHeader>
-          <CardBody className="mb-6 p-0">
-            <Typography>
-              I found solution to all my design needs from Creative Tim. I use
-              them as a freelancer in my hobby projects for fun! And its really
-              affordable, very humble guys !!!
-            </Typography>
-          </CardBody>
-        </Card>
+        <Carousel items={items} autoSlide={true} />
       </div>
     </div>
   )
