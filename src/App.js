22d055141630fc240390773f5804599948f1e530
benkis <benkisenge03@gmail.com>
2023-10-20 15:48:37 +0200

    Update app file

--- src/App.js
+++ src/App.js
@@ -1,20 +1,35 @@
+import React from 'react'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import Navbar from './components/Navbar'
 import Home from './components/Home'
 import Why from './components/Why'
 import Footer from './components/Footer'
 import Insight from './components/Insight'
 import Testimony from './components/Testimony'
+import Solutions from './pages/Solutions'
 
 function App() {
   return (
-    <div className="bg-neutral-100">
-      <Navbar />
-      <Home />
-      <Why />
-      <Insight />
-      <Testimony />
-      <Footer />
-    </div>
+    <Router>
+      <div className="bg-neutral-100">
+        <Navbar />
+        <Routes>
+          <Route path="/solutions" element={<Solutions />} />
+          <Route
+            path="/"
+            element={
+              <>
+                <Home />
+                <Why />
+                <Insight />
+                <Testimony />
+              </>
+            }
+          />
+        </Routes>
+        <Footer />
+      </div>
+    </Router>
   )
 }
 
