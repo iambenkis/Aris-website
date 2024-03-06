a60716d2ab6d69838679fcf84a9f4aee92ff0eaf
Salomon Tshobohwa <stshobohwa@gmail.com>
2024-03-06 20:11:09 +0200

    Run build command

--- src/components/Why.jsx
+++ src/components/Why.jsx
@@ -11,7 +11,7 @@ const WhySection = () => {
       <div className="mt-10">
         <Fade duration={2000} className="w-full">
           <div>
-            <div className="grid columns-1  w-full gap-8 lg:flex lg:justify-between lg:items-center bg-yellow-500">
+            <div className="grid columns-1  w-full gap-8 lg:flex lg:justify-between lg:items-center ">
               <div className="w-[100%] lg:w-[30%] border-2 shadow-lg shadow-gray-500/50 bg-white rounded-2xl text-justify p-7">
                 <h3 className="text-[1.5rem] font-[RegularNohemi] text-blue-900 font-medium">
                   100% Satisfaction
